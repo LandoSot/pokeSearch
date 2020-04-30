659fac35b92b403d7e63d2542f92fec4a3ffd805
Orlando Sotelo <danugfe@gmail.com>
2020-04-30 15:09:25 -0500

    Basic app configuration

--- App.js
+++ App.js
@@ -1,5 +1,20 @@
 // import stuff
+import React from 'react'
+import {View, Text} from 'react-native'
 
 // create stuff
+class App extends React.Component{
+  render(){
+    return(
+      <View>
+        <Text>
+          Starting project
+        </Text>
+      </View>
+    )
+  }
+}
+
 
 // export stuff
+export default App
