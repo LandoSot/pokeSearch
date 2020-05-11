1a946492778460d71741320964a70391a7a4eee9
Orlando Sotelo <danugfe@gmail.com>
2020-05-11 17:31:03 -0500

    Adding a search bar component to the search view

--- src/search.js
+++ src/search.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import {Text, View} from 'react-native'
+import {Button, Header, Icon, Input, Item} from 'native-base'
 
 export default class Search extends React.Component{
+  state = {
+    pokeSearch: ""
+  }
+  searchPoke = () => {
+
+  }
+
   render(){
     return(
-      <View>
-        <Text>Search Pokemon</Text>
+      <View style = {{flex: 1}}>
+        <Header searchBar rounded>
+          <Item>
+            <Icon name = "search" onPress = {this.searchPoke} />
+            <Input
+              value = {this.state.pokeSearch}
+              placeholder = "Search Pokemon"
+              onChangeText = {(pokeSearch) => this.setState({pokeSearch})}
+            />
+          </Item>
+        </Header>
       </View>
     )
   }
