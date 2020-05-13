5154ff8d85a0eb0042f85765bcb6a1a54b158744
Orlando Sotelo <danugfe@gmail.com>
2020-05-13 15:41:51 -0500

    Creating view for loading data

---
+++ src/pokeLoader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+
+export default class PokeLoader extends React.Component{
+  render (){
+    return(
+      <View>
+        <Text>Loading data</Text>
+      </View>
+    )
+  }
+}
